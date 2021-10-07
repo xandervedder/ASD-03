14bede6020f5426c38c847acf3ef08dcf4b5d4ca
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2021-10-07 20:15:31 +0200

    Add tests

--- src/test/java/nl/asd/reservation/application/ReservationServiceTest.java
+++ src/test/java/nl/asd/reservation/application/ReservationServiceTest.java
@@ -1,5 +1,7 @@
 package nl.asd.reservation.application;
 
+import nl.asd.reservation.ReservationNotFoundException;
+import nl.asd.reservation.domain.ReservationId;
 import nl.asd.reservation.port.adapter.FakeReservationRepository;
 import nl.asd.shared.id.WorkplaceId;
 import nl.asd.workplace.application.BuildingService;
@@ -12,10 +14,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationServiceTest {
     private ReservationService service;
+    private FakeReservationRepository repository;
 
     @BeforeEach
     public void initialize() {
-        this.service = new ReservationService(new FakeReservationRepository(), null);
+        this.repository = new FakeReservationRepository();
+        this.service = new ReservationService(this.repository, null);
     }
 
     @Test
@@ -25,5 +29,36 @@ class ReservationServiceTest {
         var to = LocalDateTime.of(2021, 12, 8, 13, 0);
 
         assertDoesNotThrow(() -> this.service.reserveWorkplace(workplace, from, to));
+    }
+
+    @Test
+    public void shouldRemoveReservationCorrectly() {
+        var workplace = new WorkplaceId(2L);
+        var from = LocalDateTime.of(2021, 12, 8, 12, 30);
+        var to = LocalDateTime.of(2021, 12, 8, 13, 0);
+        var id = this.service.reserveWorkplace(workplace, from, to);
+        this.service.removeReservation(id);
+
+        assertEquals(0, this.repository.findAll().size());
+    }
+
+    @Test
+    public void removeReservationShouldNotThrowException() {
+        var workplace = new WorkplaceId(3L);
+        var from = LocalDateTime.of(2021, 12, 8, 12, 30);
+        var to = LocalDateTime.of(2021, 12, 8, 13, 0);
+        var id = this.service.reserveWorkplace(workplace, from, to);
+
+        assertDoesNotThrow(() -> this.service.removeReservation(id));
+    }
+
+    @Test
+    public void removeNonExistingReservationShouldThrowException() {
+        var workplace = new WorkplaceId(3L);
+        var from = LocalDateTime.of(2021, 12, 8, 12, 30);
+        var to = LocalDateTime.of(2021, 12, 8, 13, 0);
+        var id = this.service.reserveWorkplace(workplace, from, to);
+
+        assertThrows(ReservationNotFoundException.class, () -> this.service.removeReservation(new ReservationId(1000L)));
     }
 }
