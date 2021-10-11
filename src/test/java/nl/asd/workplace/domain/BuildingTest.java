a3e6bc8e04b9c05dd23a0d25bc8f8c11ad4b3078
Jort Willemsen <jort@maurice-nienke.nl>
2021-10-11 16:29:49 +0200

    Write building tests

--- src/test/java/nl/asd/workplace/domain/BuildingTest.java
+++ src/test/java/nl/asd/workplace/domain/BuildingTest.java
@@ -1,43 +1,78 @@
 package nl.asd.workplace.domain;
 
+import com.sun.source.tree.AssertTree;
+import nl.asd.shared.id.BuildingId;
+import nl.asd.shared.id.WorkplaceId;
+import nl.asd.workplace.port.adapter.FakeBuildingRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingTest {
 
+    BuildingRepository buildingRepository;
+    HashMap<DayOfWeek, OpeningTime> standardOpeningHours;
+    @BeforeEach
+    void initialize() {
+        this.buildingRepository = new FakeBuildingRepository();
+
+        this.standardOpeningHours = new HashMap<DayOfWeek, OpeningTime>();
+        this.standardOpeningHours.put(DayOfWeek.MONDAY, new OpeningTime(LocalTime.of(8, 0), LocalTime.of(18, 0)));
+        this.standardOpeningHours.put(DayOfWeek.TUESDAY, new OpeningTime(LocalTime.of(8, 0), LocalTime.of(18, 0)));
+        this.standardOpeningHours.put(DayOfWeek.WEDNESDAY, new OpeningTime(LocalTime.of(8, 0), LocalTime.of(18, 0)));
+        this.standardOpeningHours.put(DayOfWeek.THURSDAY, new OpeningTime(LocalTime.of(8, 0), LocalTime.of(18, 0)));
+        this.standardOpeningHours.put(DayOfWeek.FRIDAY, new OpeningTime(LocalTime.of(8, 0), LocalTime.of(18, 0)));
+        this.standardOpeningHours.put(DayOfWeek.SATURDAY, new OpeningTime(LocalTime.of(8, 0), LocalTime.of(18, 0)));
+        this.standardOpeningHours.put(DayOfWeek.SUNDAY, new OpeningTime(LocalTime.of(8, 0), LocalTime.of(18, 0)));
+    }
+
     @Test
     void shouldCreateBuildingCorrectly() {
-        assertTrue(true);
+        var building = new Building(this.buildingRepository.nextId(), "Test Building", this.standardOpeningHours);
+        this.buildingRepository.save(building);
+
+        assertEquals(1, this.buildingRepository.findAll().size());
     }
 
     @Test
     void shouldThrowIfWorkplaceAlreadyExists() {
-        assertTrue(true);
-    }
+        var building = new Building(this.buildingRepository.nextId(), "Test Building", this.standardOpeningHours);
+        building.registerWorkplace(new Workplace(new WorkplaceId(1L), 1, 1));
 
-    @Test
-    void shouldThrowIfBuildingAlreadyExists() {
-        assertTrue(true);
+        assertThrows(RuntimeException.class, () -> building.registerWorkplace(new Workplace(new WorkplaceId(1L), 1, 1)));
     }
 
     @Test
     void shouldReturnFalseIfWorkplaceIsNotInBuilding() {
-        assertTrue(true);
+        var building = new Building(this.buildingRepository.nextId(), "Test Building", this.standardOpeningHours);
+        this.buildingRepository.save(building);
+
+        assertFalse(building.includesWorkplace(new WorkplaceId(1L)));
     }
 
     @Test
     void shouldReturnTrueIfWorkplaceIsInBuilding() {
-        assertTrue(true);
+        var building = new Building(this.buildingRepository.nextId(), "Test Building", this.standardOpeningHours);
+        building.registerWorkplace(new Workplace(new WorkplaceId(1L), 1, 1));
+        this.buildingRepository.save(building);
+
+        assertTrue(building.includesWorkplace(new WorkplaceId(1L)));
     }
 
     @Test
-    void shouldReturnFalseIfTimeIsOutsideOfOpeningHours() {
-        assertTrue(true);
+    void shouldReturnTrueIfTimeIsOutsideOfOpeningHours() {
+        var building = new Building(this.buildingRepository.nextId(), "Test Building", this.standardOpeningHours);
+        assertTrue(building.isTimeOutsideOfOpeningHoursForGivenDay(LocalTime.of(0, 0), LocalTime.of(1, 0), DayOfWeek.MONDAY));
     }
 
     @Test
-    void shouldReturnTrueIfTimeIsInsideOpeningHours() {
-        assertTrue(true);
+    void shouldReturnFalseIfTimeIsInsideOpeningHours() {
+        var building = new Building(this.buildingRepository.nextId(), "Test Building", this.standardOpeningHours);
+        assertFalse(building.isTimeOutsideOfOpeningHoursForGivenDay(LocalTime.of(9, 0), LocalTime.of(10, 0), DayOfWeek.MONDAY));
     }
 }
